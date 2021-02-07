0fd7d3dd3667c484cafe2feb16e5e2244ad02d88
Diego Cantarell <71672416+cantarell11@users.noreply.github.com>
2021-02-07 14:35:46 -0600

    Update LSLBYME.java

--- LSLBYME/src/lslbyme/LSLBYME.java
+++ LSLBYME/src/lslbyme/LSLBYME.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Autor cantarell11
 package lslbyme;
 
-/**
- *
- * @author 2RJ27LA_2004
- */
+
 public class LSLBYME {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         // TODO code application logic here
     }
